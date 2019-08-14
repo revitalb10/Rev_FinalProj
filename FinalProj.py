2d4f3de9519b83b76b1cb4dc8da258b47caff793
Revitalb10 <revitalb10@gmail.com>
2019-08-14 14:23:40 +0300

    Update FinalProj.py

--- FinalProj.py
+++ FinalProj.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import re
 
-SITE = "http://192.168.99.100:5000/"
+SITE = "http://192.168.99.102:5000/"
 
 # Use chrome as browser
 driver = webdriver.Chrome("C:\\Users\Revital\Downloads\chromedriver_win32\chromedriver.exe")
@@ -13,7 +13,7 @@ content = driver.find_element_by_xpath("/html/body").text
 
 #remove World from print
 string = re.sub('World', '', content)
-print(string)
+print(strin)
 
 
 
